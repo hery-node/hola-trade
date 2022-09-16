76ae9dd897316ff2be9d59b972c0ce22169e4cd5
Heyuan Liu <dev@dev.com>
2022-09-16 10:35:35 +0800

    change order for condition

--- src/hola_trade/trade/condition.py
+++ src/hola_trade/trade/condition.py
@@ -16,9 +16,9 @@ class Condition(ABC):
 
 
 class PolicyConditions:
-    def __init__(self, select_condition: Condition, buy_condition: Condition, sell_condition: Condition, add_condition: Condition, clear_condition: Condition) -> None:
+    def __init__(self, select_condition: Condition, buy_condition: Condition, add_condition: Condition, sell_condition: Condition,  clear_condition: Condition) -> None:
         self.select_condition = select_condition
         self.buy_condition = buy_condition
-        self.sell_condition = sell_condition
         self.add_condition = add_condition
+        self.sell_condition = sell_condition
         self.clear_condition = clear_condition
